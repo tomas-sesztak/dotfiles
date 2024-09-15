39e92c902d47736e0366c318f8e90d23f18c7fef
Tomas Sesztak <tsesztak@gmail.com>
2024-09-15 20:13:29 +0200

    nvim: trouble: added keybind to close trouble

--- .config/nvim/lua/tomas-sesztak/plugins/trouble.lua
+++ .config/nvim/lua/tomas-sesztak/plugins/trouble.lua
@@ -8,6 +8,7 @@ return {
   keys = {
     { "<leader>xx", "<cmd>Trouble<CR>", desc = "Open/close trouble list" },
     { "<leader>xd", "<cmd>Trouble diagnostics<CR>", desc = "Open trouble diagnostics" },
+    { "<leader>xc", "<cmd>Trouble diagnostics close<CR>", desc = "Close trouble diagnostics" },
     { "<leader>xq", "<cmd>Trouble quickfix<CR>", desc = "Open trouble quickfix list" },
     { "<leader>xl", "<cmd>Trouble loclist<CR>", desc = "Open trouble location list" },
   },
