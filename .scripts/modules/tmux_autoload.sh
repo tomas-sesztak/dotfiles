1f6a7a9c0471d75da1159ff4d98dc43a226d994d
Tomas Sesztak <tsesztak@gmail.com>
2024-09-15 20:09:13 +0200

    tmux: added default window name to autoload function

--- .scripts/modules/tmux_autoload.sh
+++ .scripts/modules/tmux_autoload.sh
@@ -1,10 +1,10 @@
 function tmux_load_session() {
-  source $1
-  if [ ! $SESSION_NAME = "helloworld" ]; then
-    tmux has-session -t $SESSION_NAME >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-      tmux new-session -s $SESSION_NAME -c $SESSION_PATH -d
-      tmux send -t $SESSION_NAME "$SESSION_COMMAND" Enter
+  source "$1"
+  if [ ! "$SESSION_NAME" = "helloworld" ]; then
+    tmux has-session -t "$SESSION_NAME" >/dev/null 2>&1
+    if ! tmux has-session -t "$SESSION_NAME" >/dev/null 2>&1; then
+      tmux new-session -s "$SESSION_NAME" -c "$SESSION_PATH" -n "${SESSION_WINDOW_NAME:-${SESSION_NAME}}" -d
+      tmux send -t "$SESSION_NAME" "$SESSION_COMMAND" Enter
     fi
   fi
 }
@@ -13,7 +13,7 @@ function tmux_load_session() {
 function tmux_autoload() {
   for file in ~/.config/tmux/tmux-sessions/*
   do
-    tmux_load_session $file
+    tmux_load_session "$file"
   done
 }
 
