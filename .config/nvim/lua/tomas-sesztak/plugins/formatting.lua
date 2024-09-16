8801f8cb82783adac98d79b5bc69734e9b34af07
tomas-sesztak <tsesztak@gmail.com>
2024-09-17 00:03:03 +0200

    nvim: formatter: disabled automatic activation

--- .config/nvim/lua/tomas-sesztak/plugins/formatting.lua
+++ .config/nvim/lua/tomas-sesztak/plugins/formatting.lua
@@ -1,6 +1,7 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  --event = { "BufReadPre", "BufNewFile" },
+  event = {},
   config = function()
     local conform = require("conform")
 
