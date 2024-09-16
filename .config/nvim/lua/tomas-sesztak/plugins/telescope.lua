f984f16459427e9b2ef6b38a8136c3f3325acc04
tomas-sesztak <tsesztak@gmail.com>
2024-09-17 00:28:07 +0200

    nvim: telescope: show hidden files

--- .config/nvim/lua/tomas-sesztak/plugins/telescope.lua
+++ .config/nvim/lua/tomas-sesztak/plugins/telescope.lua
@@ -28,7 +28,7 @@ return {
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" }) -- find files in cwd
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<CR>", { desc = "Fuzzy find files in cwd" }) -- find files in cwd
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" }) -- find recent files
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" }) -- find string in cwd
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in current buffer" }) -- find string under cursor in current buffer
