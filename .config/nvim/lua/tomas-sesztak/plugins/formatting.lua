a88ed8571e540270df273cccf4aed1c45bbbc3f9
tomas-sesztak <tsesztak@gmail.com>
2024-09-17 00:17:29 +0200

    nvim: formatter: disabled automatic activation - fix

--- .config/nvim/lua/tomas-sesztak/plugins/formatting.lua
+++ .config/nvim/lua/tomas-sesztak/plugins/formatting.lua
@@ -1,23 +1,23 @@
 return {
   "stevearc/conform.nvim",
-  --event = { "BufReadPre", "BufNewFile" },
-  event = {},
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     local conform = require("conform")
 
     conform.setup({
       formatters_by_ft = {
         bash = { "beautysh" },
-        sh = { "beautysh" },
-        zsh = { "beautysh" },
         lua = { "stylua" },
+        sh = { "beautysh" },
         yaml = { "prettier" },
+        zsh = { "beautysh" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      -- Disabled automatic formatting
+      --format_on_save = {
+      --  lsp_fallback = true,
+      --  async = false,
+      --  timeout_ms = 1000,
+      --},
     })
 
     local keymap = vim.keymap
