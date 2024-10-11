b8dc226d8ce21e496774eaec7d4f9116c2cb9682
tomas-sesztak <tsesztak@gmail.com>
2024-10-11 16:47:40 +0200

    removed shared register for clipboard

--- .config/nvim/lua/tomas-sesztak/core/options.lua
+++ .config/nvim/lua/tomas-sesztak/core/options.lua
@@ -29,7 +29,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, EOL or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register, enable copy&paste between neovim and system
+--opt.clipboard:append("unnamedplus") -- use system clipboard as default register, enable copy&paste between neovim and system
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
