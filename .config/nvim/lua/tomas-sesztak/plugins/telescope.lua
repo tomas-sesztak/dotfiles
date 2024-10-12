8a6dfbed4dcd17006186c2bf846d0ca51e1e18d1
tomas-sesztak <tsesztak@gmail.com>
2024-10-12 14:14:48 +0200

    Added buffer selection to telescope

--- .config/nvim/lua/tomas-sesztak/plugins/telescope.lua
+++ .config/nvim/lua/tomas-sesztak/plugins/telescope.lua
@@ -32,5 +32,6 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" }) -- find recent files
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" }) -- find string in cwd
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in current buffer" }) -- find string under cursor in current buffer
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" }) -- show active buffers
   end,
 }
