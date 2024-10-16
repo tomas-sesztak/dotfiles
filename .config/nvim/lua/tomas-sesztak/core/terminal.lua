2c0963a1314fddb9b375422b70e6ca5a1f38409f
tomas-sesztak <tsesztak@gmail.com>
2024-10-16 19:15:42 +0200

    neovim: Added keymap to return to normal mode by pressing "jk"

--- .config/nvim/lua/tomas-sesztak/core/terminal.lua
+++ .config/nvim/lua/tomas-sesztak/core/terminal.lua
@@ -1,3 +1,4 @@
 local keymap = vim.keymap
 
-keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "exit terminal" })
+keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Return to normal mode" })
+keymap.set("t", "jk", "<C-\\><C-n>", { desc = "Return to normal mode" })
