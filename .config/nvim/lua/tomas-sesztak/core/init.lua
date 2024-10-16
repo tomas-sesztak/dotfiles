1879f26d99241f339f87ccd544ba4dd9670e35bc
tomas-sesztak <tsesztak@gmail.com>
2024-10-16 13:49:43 +0200

    neovim: added config for terminal

--- .config/nvim/lua/tomas-sesztak/core/init.lua
+++ .config/nvim/lua/tomas-sesztak/core/init.lua
@@ -1,2 +1,3 @@
 require("tomas-sesztak.core.options")
 require("tomas-sesztak.core.keymaps")
+require("tomas-sesztak.core.terminal")
---
+++ .config/nvim/lua/tomas-sesztak/core/terminal.lua
@@ -0,0 +1,3 @@
+local keymap = vim.keymap
+
+keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "exit terminal" })
