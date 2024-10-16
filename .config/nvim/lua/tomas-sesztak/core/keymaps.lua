fdf106d4ff0a108bed29a8776062b6fc7c09e77c
tomas-sesztak <tsesztak@gmail.com>
2024-10-16 13:33:25 +0200

    neovim: added keymap for opening terminal in new tab

--- .config/nvim/lua/tomas-sesztak/core/keymaps.lua
+++ .config/nvim/lua/tomas-sesztak/core/keymaps.lua
@@ -21,3 +21,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- open current buffer in new tab
+
+-- open terminal in new tab
+keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>:terminal<CR>", { desc = "Open terminal in new tab" })
