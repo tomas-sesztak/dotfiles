499d860c3d414f383fa9a01db9b24aa9daa02dbb
tomas-sesztak <tsesztak@gmail.com>
2024-10-23 13:45:24 +0200

    zsh: history: 10000->50000

--- .config/zsh/plugins/tomas-sesztak/history/history.zsh
+++ .config/zsh/plugins/tomas-sesztak/history/history.zsh
@@ -1,11 +1,11 @@
 # location of the history file
 HISTFILE=~/.zsh_history
 # history limit of the file on disk
-HISTFILESIZE=10000
+HISTFILESIZE=50000
 # current session's history limit
-HISTSIZE=10000
+HISTSIZE=50000
 # zsh saves this many lines from the in-memory history list to the history file upon shell exit
-SAVEHIST=10000
+SAVEHIST=50000
 HISTTIMEFORMAT="%d/%m/%Y %H:%M] "
 
 # history file is updated immediately after a command is entered
