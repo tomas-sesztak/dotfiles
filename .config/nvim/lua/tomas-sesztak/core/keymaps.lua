c452d901f1dd78c13d06592ffa1ccc1cbe7c2030
tomas-sesztak <tsesztak@gmail.com>
2024-11-11 23:07:45 +0100

    map jk to <ESC> in visual mode
    fixes #26

--- .config/nvim/lua/tomas-sesztak/core/keymaps.lua
+++ .config/nvim/lua/tomas-sesztak/core/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set({ "i", "v" }, "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>,", ":nohl<CR>", { desc = "Clear search highlights" })
 
